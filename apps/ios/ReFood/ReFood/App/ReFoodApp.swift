07614113974eed1f5eca583ab5bece5e12c0f37e
kshashkina <kshashkina@kse.org.ua>
2026-02-13 23:44:31 +0200

    initial project setup

---
+++ apps/ios/ReFood/ReFood/App/ReFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReFoodApp.swift
+//  ReFood
+//
+//  Created by Kateryna Shashkina on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ apps/ios/ReFood/ReFood/Presentation/Root/RootView.swift
@@ -0,0 +1,24 @@
+//
+//  ReFoodApp.swift
+//  ReFood
+//
+//  Created by Kateryna Shashkina on 27.01.2026.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RootView()
+}
