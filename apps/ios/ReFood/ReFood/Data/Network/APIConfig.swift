6763f931ec4cede43e844a325c8f30f81d668206
kshashkina <kshashkina@kse.org.ua>
2026-03-31 12:02:23 +0300

    fix: change API Config

--- apps/ios/ReFood/ReFood/Data/Network/APIConfig.swift
+++ apps/ios/ReFood/ReFood/Data/Network/APIConfig.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 enum APIConfig {
-    static let baseURL = URL(string: "https://4y0ifp4smh.execute-api.eu-north-1.amazonaws.com/v2")!
+    static let baseURL = URL(string: "https://eg768eyzc1.execute-api.eu-north-1.amazonaws.com/v1")!
 }
