c46696c1a84f0ebc4c0e19e0cd291425040c7b55
kshashkina <kshashkina@kse.org.ua>
2026-05-26 22:15:03 +0300

    test: add achievements tests

--- apps/ios/ReFood/ReFoodTests/Mocks/MockHomeDependencies.swift
+++ apps/ios/ReFood/ReFoodTests/Mocks/MockHomeDependencies.swift
@@ -4,6 +4,17 @@ import Foundation
 final class MockMetricsRepository: MetricsRepositoryProtocol {
     var scannedCount = 0
     var sortedCount = 0
+    var streakCount = 1
+    var addedProductsCount = 0
+    
+    var achievementProgress: [String: (current: Int, goal: Int)] = [:]
+    var unlockedAchievements: Set<String> = []
+    
+    var incrementScannedCountCalled = false
+    var incrementSortedCountCalled = false
+    var updateStreakCalled = false
+    var trackMapCheckCalled = false
+    var trackProductAddedCalled = false
     
     func getScannedCount() -> Int {
         scannedCount
@@ -14,10 +25,37 @@ final class MockMetricsRepository: MetricsRepositoryProtocol {
     }
     
     func incrementScannedCount() {
+        incrementScannedCountCalled = true
         scannedCount += 1
     }
     
     func incrementSortedCount() {
+        incrementSortedCountCalled = true
         sortedCount += 1
+    }
+    
+    func getStreakCount() -> Int {
+        streakCount
+    }
+    
+    func updateStreak() {
+        updateStreakCalled = true
+    }
+    
+    func trackMapCheck() {
+        trackMapCheckCalled = true
+    }
+    
+    func trackProductAdded() {
+        trackProductAddedCalled = true
+        addedProductsCount += 1
+    }
+    
+    func isAchievementUnlocked(id: String) -> Bool {
+        unlockedAchievements.contains(id)
+    }
+    
+    func getAchievementProgress(id: String) -> (current: Int, goal: Int) {
+        achievementProgress[id] ?? (0, 1)
     }
 }
